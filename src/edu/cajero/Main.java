33bcbdad4aa540d8575905863541849863bfca2a
carlosubuntu <solrac.hincapie@gmail.com>
2020-04-06 19:31:48 -0500

    Se agrega un nodo

--- src/edu/cajero/Main.java
+++ src/edu/cajero/Main.java
@@ -4,7 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Java.");
-        System.out.println("Hola Bebe.");
-        System.out.println("Para el tercer commit");
+
+        Node<String> mao = new Node<String>("Magnolia");
+        Node<String> carlos = new Node<>("Carlos");
+        Node<String> santiago = new Node<>("Santiago");
+        Node<String> andres = new Node<>("Andres");
+        mao.next = carlos;
+        carlos.next = santiago;
+        santiago.next = andres;
+
+        System.out.println("El hijo de mao es: " + mao.next.getData());
+        System.out.println("El hijo de carlos es: " + mao.next.next.getData());
+        System.out.println("El hijo de santiago es: " + mao.next.next.next.getData());
+        System.out.println("El hijo de santiago es: " + mao.next.next.next.next.getData());
+
+
     }
 }
---
+++ src/edu/cajero/Node.java
@@ -0,0 +1,17 @@
+package edu.cajero;
+
+public class Node<E> {
+    private E data;
+    public Node<E> next;
+    public Node(E dataItem) {
+        data = dataItem;
+        next = null;
+    }
+    public Node(E dataItem, Node<E> nodeRef) {
+        data = dataItem;
+        next = nodeRef;
+    }
+    public E getData(){
+        return data;
+    }
+}

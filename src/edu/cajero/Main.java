3d629f1056ed369640e2c380bb3c2c6728a3678a
solrac68 <solrac.hincapie@gmail.com>
2020-05-01 13:59:05 -0500

    SE agrega test

--- src/edu/cajero/Main.java
+++ src/edu/cajero/Main.java
@@ -10,7 +10,7 @@ public class Main {
     static ListaDobleEnlazada<Cliente> listaClientes = new ListaDobleEnlazada<>();
     public static void main(String[] args) {
         //Tests.testLinkedList();
-        //Tests.testLinkedListDouble();
+        Tests.testLinkedListDouble();
         Tests.testCircularList();
 
 
